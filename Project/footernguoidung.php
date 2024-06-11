e2b83bfa7c65b58dff7a24f3c7ce9f61bea03e19
Hoang Nhan <nhanthanh535@gmail.com>
2024-06-11 17:27:53 +0700

    feat: add footer

---
+++ Project/footernguoidung.php
@@ -0,0 +1,98 @@
+<div class="footer">
+            <div class="grid wide">
+                <div class="row">
+                    <div class="col l-5">
+                        <div class="footer-items__first">
+                            <img src="assets/img/logo__footer.png" alt="" class="footer-items__logo">
+                            <p class="footer-items__intro">Fashion and accessories shop HN SHOP</p>
+                            <div class="footer-items__address">
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon footer-items__address-icon-strong fa-solid fa-location-dot"></i>
+                                    <p>158A, Le Loi, Hai Chau District, Da Nang City</p>
+                                </div>
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon fa-regular fa-clock"></i>
+                                    <p>Working hours: From 9:00 to 22:00 weekdays from Monday to Sunday</p>
+                                </div>
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon fa-solid fa-phone"></i>
+                                    <p class="footer-bottom__call-msg-text">
+                                        Phone number:
+                                        <a href="tel:0764 481 906" class="footer-bottom__call-msg-num">0931 937 000</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col l-7">
+                        <div class="row">
+                            <div class="col l-4">
+                                <div class="footer-items__last">
+                                    <h4 class="footer-items__heading">About us</h4>
+                                    <ul class="footer-items__list">
+                                        <li class="footer-items__info">
+                                            <a href="index_home.php" class="footer-items__info-links">Home page</a>
+                                        </li>
+                                        <div class="footer-items__info">
+                                            <a href="all.php" class="footer-items__info-links">Product</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="index_news.php" class="footer-items__info-links">News</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="contact.php" class="footer-items__info-links">Contact</a>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col l-4">
+                                <div class="footer-items__last">
+                                    <h4 class="footer-items__heading">Customer support</h4>
+                                    <ul class="footer-items__list">
+                                        <li class="footer-items__info">
+                                            <a href="index_home.php" class="footer-items__info-links">Home page</a>
+                                        </li>
+                                        <div class="footer-items__info">
+                                            <a href="all.php" class="footer-items__info-links">Product</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="index_news.php" class="footer-items__info-links">News</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="contact.php" class="footer-items__info-links">Contact</a>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col l-4">
+                                <div class="footer-items__last">
+                                    <h4 class="footer-items__heading">Service</h4>
+                                    <ul class="footer-items__list">
+                                        <li class="footer-items__info">
+                                            <a href="index_home.php" class="footer-items__info-links">Home page</a>
+                                        </li>
+                                        <div class="footer-items__info">
+                                            <a href="all.php" class="footer-items__info-links">Product</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="index_news.php" class="footer-items__info-links">News</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="contact.php" class="footer-items__info-links">Contact</a>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <span class="footer-copyright__text">© Copyright belongs to Hoang Nhan Bui</span>
+        </div>
+
+    </div>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
